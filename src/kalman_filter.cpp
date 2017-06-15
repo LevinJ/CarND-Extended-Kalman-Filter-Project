fc16b5b61e2fa5192acbafa8a5f19b81d3727267
Levin Jian <jianzhirong@gmail.com>
2017-06-15 17:23:08 +0800

    refractor kalman.cpp

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -10,107 +10,91 @@ KalmanFilter::KalmanFilter() {}
 KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+		MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+	x_ = x_in;
+	P_ = P_in;
+	F_ = F_in;
+	H_ = H_in;
+	R_ = R_in;
+	Q_ = Q_in;
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
-	
+
 	x_ = F_ * x_;
-	
+
 	MatrixXd Ft = F_.transpose();
-	
+
 	P_ = F_ * P_ * Ft + Q_;
-	
+
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
-	
-		VectorXd z_pred = H_ * x_;
-		
-		VectorXd y = z - z_pred;
-		
-		MatrixXd Ht = H_.transpose();
-		
-		MatrixXd S = H_ * P_ * Ht + R_;
-		
-		MatrixXd Si = S.inverse();
-		
-		MatrixXd PHt = P_ * Ht;
-		
-		MatrixXd K = PHt * Si;
 
-		//new estimate
-		
-		x_ = x_ + (K * y);
-		
-		long x_size = x_.size();
-		
-		MatrixXd I = MatrixXd::Identity(x_size, x_size);
-		
-		P_ = (I - K * H_) * P_;
+	VectorXd z_pred = H_ * x_;
+
+	VectorXd y = z - z_pred;
+
+	MatrixXd Ht = H_.transpose();
+
+	MatrixXd S = H_ * P_ * Ht + R_;
+
+	MatrixXd Si = S.inverse();
+
+	MatrixXd PHt = P_ * Ht;
+
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+
+	x_ = x_ + (K * y);
+
+	long x_size = x_.size();
+
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
-	//cout << z << endl << endl;
 
-		//VectorXd z_pred = H_ * x_;
 	VectorXd z_pred = VectorXd(3);
 	float ro;
 	float theta;
 	float ro_dot;
 	ro = sqrt(x_[0]*x_[0] + x_[1]*x_[1]);
-	//ro_dot = sqrt(x_[2]*x_[2] + x_[3]*x_[3]);
 	ro_dot = (x_[0]*x_[2] + x_[1]*x_[3])/ro;
 	theta = atan2(x_[1],x_[0]);
-	
-	z_pred << ro, theta, ro_dot;
-		VectorXd y = z - z_pred;
-		
-		while (y(1) > M_PI)
-			{
-				y(1) -= 2 * M_PI;
-			}
-			while (y(1) < -M_PI)
-			{
-				y(1) += 2 * M_PI;
-			}
-			
-		//cout << x_[0] << "," << x_[1] << endl;
-		MatrixXd Ht = H_.transpose();
-		
-		MatrixXd S = H_ * P_ * Ht + R_;
-		
-		MatrixXd Si = S.inverse();
-		
-		MatrixXd PHt = P_ * Ht;
-		
-		MatrixXd K = PHt * Si;
 
-		//new estimate
-		
-		x_ = x_ + (K * y);
-		
-		long x_size = x_.size();
-		
-		MatrixXd I = MatrixXd::Identity(x_size, x_size);
-		
-		P_ = (I - K * H_) * P_;
+	z_pred << ro, theta, ro_dot;
+	VectorXd y = z - z_pred;
+
+	while (y(1) > M_PI)
+	{
+		y(1) -= 2 * M_PI;
+	}
+	while (y(1) < -M_PI)
+	{
+		y(1) += 2 * M_PI;
+	}
+
+
+	MatrixXd Ht = H_.transpose();
+
+	MatrixXd S = H_ * P_ * Ht + R_;
+
+	MatrixXd Si = S.inverse();
+
+	MatrixXd PHt = P_ * Ht;
+
+	MatrixXd K = PHt * Si;
+
+
+	x_ = x_ + (K * y);
+
+	long x_size = x_.size();
+
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+
+	P_ = (I - K * H_) * P_;
 }
